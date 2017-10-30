0c22b3e8b4d729f7f0c0182d0a4e1e39603e6e1d
Radhika Raghu <radhu.r@gmail.com>
2017-10-30 12:22:13 +0530

    Add files via upload

--- category.js
+++ category.js
@@ -1,4 +1,3 @@
-
 var ref=firebase.database().ref();
 var cat=document.getElementById("cate");
 var submitBtn=document.getElementById("submitBtn");
@@ -20,3 +19,25 @@ ret.on("child_added",snap =>{
 
 });
 
+var q=document.getElementById("Ques");
+var submitBtnQ=document.getElementById("submitBtnQ");
+
+function addQ(){
+
+	 var mcat=q.value;
+	 ref.child("Questions").push().set(mcat);
+	 $('#AddQuestion').modal('hide');
+	 // window.alert("Done");
+}
+
+var ret = firebase.database().ref().child("Questions");
+
+ret.on("child_added",snap =>{
+
+	var question = snap.val();
+	$('#').append("<li><a>"+question+"</a></li>");
+
+});
+
+
+	
